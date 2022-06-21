03b5ee653cfcfe32e414b34584df9590406babeb
hutchmed <phani.kumar@irissoftware.com>
2022-06-21 14:30:10 +0530

    added Product card component

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,11 +1,16 @@
-import { Fragment } from 'react';
+import { Fragment, useContext } from 'react';
 import { Outlet, Link } from 'react-router-dom';
+import { UserContext } from '../../contexts/user.context';
+import { userSignOut } from '../../utils/firebase/firebase.utils';
+
 
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 
 import './navigation.styles.scss';
+import { async } from '@firebase/util';
 
 const Navigation = () => {
+  const {currentUser} = useContext(UserContext);
   return (
     <Fragment>
       <div className='navigation'>
@@ -16,9 +21,10 @@ const Navigation = () => {
           <Link className='nav-link' to='/shop'>
             SHOP
           </Link>
-          <Link className='nav-link' to='/auth'>
-            SIGN IN
-          </Link>
+         {
+          (currentUser ? <span className='nav-link' onClick={userSignOut}>SIGN OUT</span> :
+          <Link className='nav-link' to='/auth'> SIGN IN </Link>)
+         }
         </div>
       </div>
       <Outlet />
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -6,16 +6,18 @@ import {
   GoogleAuthProvider,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged
 } from 'firebase/auth';
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
-  authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
-  projectId: 'crwn-clothing-db-98d4d',
-  storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
-  messagingSenderId: '626766232035',
-  appId: '1:626766232035:web:506621582dab103a4d08d6',
+  apiKey: "AIzaSyBqeA2Y-bywHPLU7OS_ISPdNXkGGQL-048",
+  authDomain: "jahir-clothing-db.firebaseapp.com",
+  projectId: "jahir-clothing-db",
+  storageBucket: "jahir-clothing-db.appspot.com",
+  messagingSenderId: "28461781293",
+  appId: "1:28461781293:web:80734d1b330983b20843a4"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
@@ -74,3 +76,11 @@ export const signInAuthUserWithEmailAndPassword = async (email, password) => {
 
   return await signInWithEmailAndPassword(auth, email, password);
 };
+
+export const userSignOut = async () => {
+  return await signOut(auth);
+}
+
+export const onAuthStateChangedListener = (callback) => {
+  return onAuthStateChanged(auth, callback);
+}
